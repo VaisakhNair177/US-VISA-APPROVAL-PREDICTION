afe0f38d0eb28c8fa91cbec20039fa4c8add5af6
VaisakhNair177 <128241168+VaisakhNair177@users.noreply.github.com>
2024-03-29 21:39:23 +0530

    Updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -68,3 +68,6 @@ MODEL_BUCKET_NAME = "usvisa-model2024v"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
+
--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -33,6 +33,9 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
+        
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
